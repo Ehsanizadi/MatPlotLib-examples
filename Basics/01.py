c6a9a09d361797f0bc4233f8bb262beb6ea338d1
Ehsanizadi <Ehsanizadi@outlook.com>
2015-11-23 11:36:04 +0100

    the very very simplest graph has been added

---
+++ Basics/01.py
@@ -0,0 +1,10 @@
+# In this section the basics of usage of matplotlib will be dealt with.
+
+import matplotlib.pyplot as plt
+
+# the base form
+# plt.plot(  x          ,   y  )
+plt.plot ( [0,1,2,3,4,5], [0.14,0.25,0.35,0.3,0.32,0.47])
+
+# it shows the plot
+plt.show()
