3c3fa4706cf92fa38664397fd72776ca670aa7f5
Ehsanizadi <Ehsanizadi@outlook.com>
2015-11-25 09:49:29 +0100

    Template folder is made for normal usage of plots, and the files renamed to be more self-explanatory

---
+++ Basics/10-basic customizations.py
@@ -0,0 +1,18 @@
+#  Basic costimizations
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# You have to declare the same number f variables as you have in the text file
+# 'skiprows' ignores the header (the first line)
+time, shearingDisp, verticalDisp, fsf, rsf, totalForce, shearStress, manifold, contact, CN = np.loadtxt('data.txt', delimiter=' ', unpack=True, skiprows=1)
+
+plt.plot(shearingDisp, shearStress, label='Shear stress (kPa)', linewidth=2, marker='o', markersize=10, markeredgewidth= 2, markeredgecolor='k', markerfacecolor='y')
+
+
+plt.xlabel('Shear deformation (mm)')
+plt.ylabel('Shear stress (GPa)')
+plt.title('The stress-strain behaviour of soil sample]')
+
+plt.legend()
+plt.show()
