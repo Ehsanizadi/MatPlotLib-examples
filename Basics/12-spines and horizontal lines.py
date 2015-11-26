8561f8c0d013450bd7839b320af477f195240811
Ehsanizadi <Ehsanizadi@outlook.com>
2015-11-26 11:41:55 +0100

    filling and coloring custimizations done, to spines and horizontal lines

--- Basics/12-spines and horizontal lines.py
+++ Basics/12-spines and horizontal lines.py
@@ -1,4 +1,4 @@
-#  More customization of colors and fills 
+#  Spines and horizontal lines
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,9 +9,6 @@ fig = plt.figure()
 ax1 = plt.subplot2grid((1,1),(0,0))
 
 plt.plot(shearingDisp, shearStress, label='Shear stress (kPa)', color='k', linewidth=2, marker='x', markersize=8, markeredgewidth= 1, markeredgecolor='k', markerfacecolor='b')
-#  To fill the subsurface of the plot (the 3rd parameter indicates the base line for filling) (Alpha indicates the degree of the opaqueness)
-ax1.fill_between(shearingDisp, shearStress, 70 , alpha=0.3)
-
 
 plt.xlabel('Shear deformation (mm)')
 plt.ylabel('Shear stress (GPa)')
@@ -19,13 +16,6 @@ plt.ylabel('Shear stress (GPa)')
 plt.subplots_adjust(left=0.1, bottom= 0.1, right= 0.95, top=0.95, wspace=0.1, hspace= 0.1)
 
 ax1.grid(True,color='grey', linestyle=':')
-
-# To change the color of the axes labels
-ax1.xaxis.label.set_color('r')
-ax1.yaxis.label.set_color('c')
-
-# To set the tick marks as specific as you want:
-ax1.set_yticks([0,50,100,150,200])
 
 ax1.legend()
 plt.show()
