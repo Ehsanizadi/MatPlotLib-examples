47f9b2fbab6d623a671d30666f787a7ae0707550
Ehsanizadi <Ehsanizadi@outlook.com>
2015-11-26 12:35:22 +0100

    checkpoint

--- Basics/12-spines and horizontal lines.py
+++ Basics/12-spines and horizontal lines.py
@@ -12,6 +12,27 @@ plt.plot(shearingDisp, shearStress, label='Shear stress (kPa)', color='k', linew
 
 plt.xlabel('Shear deformation (mm)')
 plt.ylabel('Shear stress (GPa)')
+
+# To change the characteristics of each axis line, thickness, color and ticks
+ax1.spines['left'].set_color('#3a3a3a')
+ax1.spines['right'].set_color('#3a3a3a')
+ax1.spines['bottom'].set_color('#3a3a3a')
+ax1.spines['top'].set_color('#3a3a3a')
+
+# ax1.spines['top'].set_visible(False)
+
+ax1.spines['left'].set_linewidth('2.5')
+ax1.spines['bottom'].set_linewidth('2.5')
+ax1.spines['right'].set_linewidth('2.5')
+ax1.spines['top'].set_linewidth('2.5')
+
+ax1.tick_params(axis='x', colors='#3a3a3a')
+ax1.tick_params(axis='y', colors='#3a3a3a')
+
+# Adding a horizontal line (1st parameter is the position of hline)
+ax1.axhline(50, color= '#3a3a3a', linewidth=2)
+
+
 # plt.title('The stress-strain behaviour of soil sample]')
 plt.subplots_adjust(left=0.1, bottom= 0.1, right= 0.95, top=0.95, wspace=0.1, hspace= 0.1)
 
