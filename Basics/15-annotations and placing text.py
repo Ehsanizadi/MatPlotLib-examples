661f8cb7ca6c2041fc5da34fb140ada89c00ad8b
Ehsanizadi <Ehsanizadi@outlook.com>
2015-11-27 13:26:26 +0100

    annotations and placing text started

---
+++ Basics/15-annotations and placing text.py
@@ -0,0 +1,46 @@
+#  Annotations and placing text on graph
+import matplotlib.pyplot as plt
+import numpy as np
+#  add this line in order to access the "ready-to-use" styles of matplotlib
+from matplotlib import style
+
+# You can pick the style you prefer
+# style.use('ggplot')
+style.use('fivethirtyeight')
+
+
+time, shearingDisp, verticalDisp, fsf, rsf, totalForce, shearStress, manifold, contact, CN = np.loadtxt('../Text files (data)/data.txt', delimiter=' ', unpack=True, skiprows=1)
+
+fig = plt.figure()
+
+ax1 = plt.subplot2grid((1,1),(0,0))
+
+plt.plot(shearingDisp, shearStress, label='Shear stress (kPa)', color='k', linewidth=2, marker='x', markersize=8, markeredgewidth= 1, markeredgecolor='k', markerfacecolor='b')
+
+plt.xlabel('Shear deformation (mm)')
+plt.ylabel('Shear stress (GPa)')
+
+# To change the characteristics of each axis line, thickness, color and ticks
+ax1.spines['left'].set_color('#3a3a3a')
+ax1.spines['right'].set_color('#3a3a3a')
+ax1.spines['bottom'].set_color('#3a3a3a')
+ax1.spines['top'].set_color('#3a3a3a')
+
+# ax1.spines['top'].set_visible(False)
+
+ax1.spines['left'].set_linewidth('2.5')
+ax1.spines['bottom'].set_linewidth('2.5')
+ax1.spines['right'].set_linewidth('2.5')
+ax1.spines['top'].set_linewidth('2.5')
+
+ax1.tick_params(axis='x', colors='#3a3a3a')
+ax1.tick_params(axis='y', colors='#3a3a3a')
+
+
+# plt.title('The stress-strain behaviour of soil sample]')
+plt.subplots_adjust(left=0.1, bottom= 0.1, right= 0.95, top=0.95, wspace=0.1, hspace= 0.1)
+
+ax1.grid(True,color='grey', linestyle=':')
+
+ax1.legend()
+plt.show()
