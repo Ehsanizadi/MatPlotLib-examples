429a4ed551dda71d5581d4461a00f7a03b8802c0
Ehsanizadi <Ehsanizadi@outlook.com>
2015-12-02 14:47:42 +0100

    template finished!

--- Templates/membraneStrain/plot.py
+++ Templates/membraneStrain/plot.py
@@ -8,7 +8,8 @@ import numpy as np
 # Initial settings
 fig1 = plt.figure(facecolor='white')
 
-x, y, z, deforamtion = np.loadtxt('membraneStrain-initial.csv', delimiter=',', unpack=True, skiprows=1)
+# Settings for membrane prior to shearing
+x, y, z, deforamtion = np.loadtxt('membraneStrain-final.csv', delimiter=',', unpack=True, skiprows=1)
 
 membraneRadius = 25.8
 Z=z
@@ -26,10 +27,39 @@ zGrid = griddata(X, z, deforamtion, xGrid, yGrid, interp='linear')
 
 
 # contour the gridded data, plotting dots at the nonuniform data points.
-CS = plt.contour(xGrid, yGrid, zGrid, 10, linewidths=0.1, colors='grey')
-CS = plt.contourf(xGrid, yGrid, zGrid, 10, cmap=plt.cm.RdBu,
+ax1 = plt.contour(xGrid, yGrid, zGrid, 15, linewidths=0.1, colors='grey')
+ax1 = plt.contourf(xGrid, yGrid, zGrid, 15, cmap=plt.cm.RdBu,
                   vmax=abs(zGrid).max(), vmin=-abs(zGrid).max())
 plt.colorbar()  # draw colorbar
+plt.title('membrane at the end of shearing')
+
+
+# for final situation of membrane
+fig2 = plt.figure(facecolor='white')
+xf, yf, zf, deforamtionf = np.loadtxt('membraneStrain-initial.csv', delimiter=',', unpack=True, skiprows=1)
+
+Zf=zf
+Xf=xf
+# making X of the plot
+for i in range(0,len(xf)): 
+	Xf[i] =0 + (i%80) *2.026327262
+
+
+# Making grid
+xGridf = np.linspace(0, 162, 80)
+yGridf = np.linspace(-10, 105, 80)
+# grid the data.
+zGridf = griddata(Xf, zf, deforamtionf, xGridf, yGridf, interp='linear')
+
+
+# contour the gridded data, plotting dots at the nonuniform data points.
+ax1 = plt.contour(xGridf, yGridf, zGridf, 15, linewidths=0.1, colors='grey')
+ax1 = plt.contourf(xGridf, yGridf, zGridf, 15, cmap=plt.cm.RdBu,
+                  vmax=abs(zGrid).max(), vmin=-abs(zGrid).max())
+plt.colorbar()  # draw colorbar
+plt.title('membrane prior to shearing')
+
+
 
 plt.show()
 
